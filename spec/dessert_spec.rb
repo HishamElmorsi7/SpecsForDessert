9718675c0866ce20c7d1817b06cb7b56d45fc93f
HishamElmorsi7 <hi7u3xe@gmail.com>
2022-10-03 07:37:11 +0200

    Added specs for initialize method

--- spec/dessert_spec.rb
+++ spec/dessert_spec.rb
@@ -6,16 +6,29 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:dessert) { Dessert.new('brownie', 100, 'Adam') }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
 
-    it "sets a quantity"
+    it "sets a type" do
+      expect(dessert.type).to eq('brownie')
+    end
 
-    it "starts ingredients as an empty array"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(100)
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+
+      expect { Dessert.new('brownie', 'kilo', 'Adam')}.to raise_error(ArgumentError)
+
+    end
+
   end
 
   describe "#add_ingredient" do
